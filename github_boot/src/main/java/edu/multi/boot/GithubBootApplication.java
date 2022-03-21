079bb4285af958fecb76acf0eae664d42862ba3a
Saeha Lee <togk1996@gmail.com>
2022-03-21 15:24:45 +0900

    Update GithubBootApplication.java

--- github_boot/src/main/java/edu/multi/boot/GithubBootApplication.java
+++ github_boot/src/main/java/edu/multi/boot/GithubBootApplication.java
@@ -9,5 +9,6 @@ public class GithubBootApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GithubBootApplication.class, args);
 		System.out.println("깃헙push용도");
+		System.out.println("원격저장소 추가 라인");	
 	}
 }
